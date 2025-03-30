f0b1630df30fd1735f9b9df3cad4328415f6e3dc
Karel-Brecht <karel.brecht.decorte@gmail.com>
2025-03-30 04:19:36 +0200

    optional argument and replace old video

--- app/downloader.py
+++ app/downloader.py
@@ -1,9 +1,11 @@
+import os
 import yt_dlp
 
 DOWNLOAD_PATH = 'downloads/videos'
 FILENAME = 'download.mp4'
+PATH_DOWNLOAD_FILE = f'{DOWNLOAD_PATH}/{FILENAME}'
 YDL_OPTIONS = {
-    'outtmpl': f'{DOWNLOAD_PATH}/{FILENAME}',
+    'outtmpl': PATH_DOWNLOAD_FILE,
 }
 
 def download_video(video_url: str) -> str:
@@ -11,7 +13,7 @@ def download_video(video_url: str) -> str:
     Downloads a YouTube video at the highest available resolution and saves it as an MP4 file.
 
     Args:
-        url (str): The URL of the YouTube video.
+        video_url (str): The URL of the YouTube video.
     
     Returns:
         str: The file path of the downloaded video.
@@ -19,10 +21,32 @@ def download_video(video_url: str) -> str:
     Raises:
         ValueError: If the video cannot be downloaded.
     """
+    # First delete the file if it exists
+    delete_video()
+
+    # Now download the video
     with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
         try:
             info_dict = ydl.extract_info(video_url, download=True)
-            file_name = ydl.prepare_filename(info_dict)
-            return file_name
+            file_path = ydl.prepare_filename(info_dict)
+            return file_path
         except Exception as e:
             raise ValueError(f"Error downloading video: {e}")
+
+def delete_video():
+    """
+    Deletes the downloaded video file if it exists.
+
+    Returns:
+        bool: True if the file was deleted, False if it did not exist.
+    Raises:
+        ValueError: If there was an error deleting the file.
+    """
+    if os.path.exists(PATH_DOWNLOAD_FILE):
+        try:
+            os.remove(PATH_DOWNLOAD_FILE)
+            return True
+        except Exception as e:
+            raise ValueError(f"Error deleting existing file: {e}")
+    else:
+        return False
--- app/main.py
+++ app/main.py
@@ -1,4 +1,20 @@
+import argparse
 from downloader import download_video
 
-download_path = download_video("https://www.youtube.com/watch?v=swavE6ZwLJQ")
-print(f"Video downloaded to: {download_path}")
+
+DEFAULT_YT_URL = "https://www.youtube.com/watch?v=swavE6ZwLJQ"
+
+def main():
+
+    # Optional youtube video URL
+    parser = argparse.ArgumentParser(description="Download and process a YouTube video.")
+    parser.add_argument('yt_url', type=str, nargs='?', default=DEFAULT_YT_URL, help = "Optional YouTube video URL")
+    args = parser.parse_args()
+    yt_url = args.yt_url
+    print(f"Downloading video from: {yt_url}")
+
+    download_path = download_video(yt_url)
+    print(f"Video downloaded to: {download_path}")
+
+if __name__ == "__main__":
+    main()

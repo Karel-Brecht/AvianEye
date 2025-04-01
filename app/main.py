ee2ee157628ffddbffbf001688852c00a50dad38
Karel-Brecht Decorte <karel.brecht.decorte@gmail.com>
2025-04-01 16:13:36 +0200

    -o output argument main

--- app/main.py
+++ app/main.py
@@ -13,14 +13,19 @@ from app.models.video_processor import VideoProcessor
 
 DEFAULT_YT_URL = "https://www.youtube.com/watch?v=swavE6ZwLJQ"
 
+DEFAULT_OUTPUT_PATH = "./outputvidrun.mp4"
+
 def main():
 
     # Optional youtube video URL
     parser = argparse.ArgumentParser(description="Download and process a YouTube video.")
     parser.add_argument('yt_url', type=str, nargs='?', default=DEFAULT_YT_URL, help = "Optional YouTube video URL")
+    parser.add_argument('-o', '--output', type=str, nargs='?', default=DEFAULT_OUTPUT_PATH, help="Optional output file path")
     args = parser.parse_args()
     yt_url = args.yt_url
+    output_path = args.output
     print(f"Downloading video from: {yt_url}")
+    print(f"Output will be saved to: {output_path}")
 
     downloader = Downloader("downloads/videos")
     detector = BirdDetector("models/detection_model/yolov10n.pt")
@@ -28,7 +33,7 @@ def main():
 
     vp = VideoProcessor(yt_url, downloader, detector, classfier)
 
-    vp.run("./outputvidrun.mp4", frame_ids=True)
+    vp.run(output_path, frame_ids=True)
 
 if __name__ == "__main__":
     main()

1543207ac6eedfa3e6a405a7aea2bf110a72a140
Karel-Brecht Decorte <karel.brecht.decorte@gmail.com>
2025-04-01 15:11:28 +0200

    move varia files into notebooks folder

--- notebooks/bird_detection/bird_detect.py
+++ notebooks/bird_detection/bird_detect.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Add project root directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from app.operations.detection import BirdDetector
 
